2885b342e26dfab77018afc1fceb62f76fe93650
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-05 23:29:00 +0300

    feat: create empty controller for auth

---
+++ authController.js
@@ -0,0 +1,27 @@
+class AuthController {
+    async registration(req, res) {
+        try{
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async login(req, res) {
+        try{
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async setUsers(req, res) {
+        try{
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+module.exports = new AuthController();
