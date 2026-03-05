c9d6028e1127b28b60553df1010744137ca19482
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-06 00:11:40 +0300

    feat: add express-validator to validate credentials

--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express');
 const router = new Router();
 const controller = require('./authController');
+const {check} = require('express-validator');
 
-router.post('/registration', controller.registration);
+router.post('/registration', [
+    check('username', 'Username can\'t be empty').trim().notEmpty(),
+    check('password', 'Password length must be in range 4 and 10').trim().isLength({min: 4, max: 10})
+], controller.registration);
 router.post('/login', controller.login);
 router.get('/users', controller.getUsers);
 
