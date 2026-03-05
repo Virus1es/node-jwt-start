f03659d6b6c64137c0ec6247a70f2d9878692738
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-05 23:56:23 +0300

    feat: first version of registration logic

--- authController.js
+++ authController.js
@@ -1,9 +1,22 @@
+const User = require('./models/User');
+const Role = require('./models/Role');
+const bcrypt = require('bcryptjs');
+
 class AuthController {
     async registration(req, res) {
         try{
-
+            const {username, password} = req.body;
+            const candidate = await User.findOne({username});
+            if(candidate){
+                return res.status(400).json({message: 'User already exists'});
+            }
+            const hashPassword = bcrypt.hashSync(password, 7);
+            const userRole = await Role.findOne({value: "USER"});
+            await User.create({username, password: hashPassword, roles: [userRole.value]});
+            return res.json({message: 'User create is success'});
         } catch (e) {
             console.log(e);
+            res.status(400).json({message: 'Registration error'});
         }
     }
 
@@ -12,12 +25,13 @@ class AuthController {
 
         } catch (e) {
             console.log(e);
+            res.status(400).json({message: 'Login error'});
         }
     }
 
     async getUsers(req, res) {
         try{
-
+            res.json(await User.find());
         } catch (e) {
             console.log(e);
         }
