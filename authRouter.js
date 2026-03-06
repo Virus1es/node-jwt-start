935cfddb2c2acd41fe648a6a7f4a4df279c90fe3
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-06 21:36:58 +0300

    feat: close access to users list for anyone without role USER

--- authRouter.js
+++ authRouter.js
@@ -3,12 +3,13 @@ const router = new Router();
 const controller = require('./authController');
 const {check} = require('express-validator');
 const authMiddleware = require('./middleware/authMiddleware');
+const roleMiddleware = require('./middleware/roleMiddleware');
 
 router.post('/registration', [
     check('username', 'Username can\'t be empty').trim().notEmpty(),
     check('password', 'Password length must be in range 4 and 10').trim().isLength({min: 4, max: 10})
 ], controller.registration);
 router.post('/login', controller.login);
-router.get('/users', authMiddleware, controller.getUsers);
+router.get('/users', roleMiddleware(["USER"]), controller.getUsers);
 
 module.exports = router;
