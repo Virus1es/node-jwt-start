1971498aa8670480a55521cd7a7471006ad4a35c
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-06 21:26:35 +0300

    feat: get list of users can only login user

--- authRouter.js
+++ authRouter.js
@@ -2,12 +2,13 @@ const Router = require('express');
 const router = new Router();
 const controller = require('./authController');
 const {check} = require('express-validator');
+const authMiddleware = require('./middleware/authMiddleware');
 
 router.post('/registration', [
     check('username', 'Username can\'t be empty').trim().notEmpty(),
     check('password', 'Password length must be in range 4 and 10').trim().isLength({min: 4, max: 10})
 ], controller.registration);
 router.post('/login', controller.login);
-router.get('/users', controller.getUsers);
+router.get('/users', authMiddleware, controller.getUsers);
 
 module.exports = router;
